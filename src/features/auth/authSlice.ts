8b07ac519d62fac7ac9d52067bfa2ca889bdfca1
LHFahim <fahimlutful@gmail.com>
2024-06-04 20:03:22 +0600

    added register feature

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,13 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const authInitialState = {};
+const authInitialState = {
+  accessToken: undefined,
+  user: undefined,
+};
 
 const authSlice = createSlice({
   name: "auth",
   initialState: authInitialState,
-  reducers: {},
+  reducers: {
+    userLoggedIn: (state, action) => {
+      state.accessToken = action.payload.accessToken;
+      state.user = action.payload.user;
+    },
+    userLoggedOut: (state) => {
+      state.accessToken = undefined;
+      state.user = undefined;
+    },
+  },
 });
 
 // export default authSlice.reducer;
 export const authSliceReducer = authSlice.reducer;
-// export const {} = authSlice.actions;
+export const { userLoggedIn, userLoggedOut } = authSlice.actions;
